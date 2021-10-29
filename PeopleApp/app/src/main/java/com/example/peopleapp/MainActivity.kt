d5a28509c6ca33424fa16a3fb176d6df61e7b652
Hardik Sachan <sachanhardik@gmail.com>
2021-10-29 13:48:08 +0530

    android: Init project

---
+++ PeopleApp/app/src/main/java/com/example/peopleapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.peopleapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.peopleapp.ui.theme.PeopleAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PeopleAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PeopleAppTheme {
+        Greeting("Android")
+    }
+}
