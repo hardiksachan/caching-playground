96124e7056c1288e72ca4b262d197fa8392ea917
Hardik Sachan <sachanhardik@gmail.com>
2021-10-29 23:38:45 +0530

    android: setup dependencies

--- PeopleApp/app/src/main/java/com/example/peopleapp/MainActivity.kt
+++ PeopleApp/app/src/main/java/com/example/peopleapp/MainActivity.kt
@@ -8,31 +8,18 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.peopleapp.ui.theme.PeopleAppTheme
+import com.example.peopleapp.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PeopleAppTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Text("Hello Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PeopleAppTheme {
-        Greeting("Android")
     }
 }
